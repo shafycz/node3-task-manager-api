c89bcbb8e020c8e0f895b6b806fdbb5d63ff07f9
shafycz <shafycz@udel.edu>
2019-05-14 16:42:17 -0400

    refactoring using bcryptjs and jsonwebtoken

--- src/index.js
+++ src/index.js
@@ -13,3 +13,15 @@ app.use(taskRouter)
 app.listen(port, ()=>{
     console.log('Server is up on port '+ port)
 })
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () =>{
+    const token =jwt.sign({_id:'abc123'},'thisismynewcourse',{expiresIn:'5 seconds'})
+    console.log(token)
+
+   const data = jwt.verify(token,'thisismynewcourse')
+   console.log(data)
+}
+
+myFunction()
--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,15 @@ router.post('/users', async (req,res)=>{
     }
 })
 
+router.post('/users/login',async (req,res) =>{
+    try{
+        const user = await User.findByCredentials(req.body.email,req.body.password)
+        res.send(user)
+    }catch(e){
+            res.status(400).send()
+    }
+})
+
 router.get('/users',async (req,res)=>{
 
     try{
@@ -48,7 +57,12 @@ router.patch('/users/:id' , async (req,res)=>{
     }
 
     try{
-        const user = await User.findByIdAndUpdate(_id,req.body,{new:true,runValidators: true })
+        const user = await User.findById(_id)
+        updates.forEach((update)=> user[update] = req.body[update])
+
+        await user.save()
+        //const user = await User.findByIdAndUpdate(_id,req.body,{new:true,runValidators: true })
+
         if(!user){
             return res.status(400).send()
         }
