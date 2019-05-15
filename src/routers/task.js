541b7b83b2da2df432800dc2ac3e77a15f4ad2e8
shafycz <shafycz@udel.edu>
2019-05-15 13:18:04 -0400

    added filtering and pagination to get task endpoint

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,10 +18,32 @@ router.post('/tasks',auth,async (req,res)=>{
         res.status(400).send(e)
     }
 })
-
+//GET /tasks?completed
+//GET /tasks?limit&skip=0
+//GET /tasks?sortBy=createdAt_asc
 router.get('/tasks',auth,async (req,res)=>{
+    const match = {}
+    const sort = {}
+
+    if(req.query.completed){
+        match.completed = req.query.completed ==='true'
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split('_')
+        sort[parts[0]] = parts[[1]] ==='desc' ?-1: 1
+    }
+
     try{
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options:{
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     }catch(e){
         res.status(500).send
